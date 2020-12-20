599b1ca5d83d8a2711b51b944cbdcdd1b9978807
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2020-12-19 21:05:53 -0500

    Delete ContactTest.java

--- CS-320/Project1/ContactTest.java
+++
@@ -1,9 +0,0 @@
-public class ContactTest 
-{
-	 public static void main(String[] args) throws InvalidCheck 
-	 {
-	      //insert new contact and verify results
-	       Contact firstContact =new Contact("1","Tyler","Bullock","7278675309","Florida");
-	       System.out.println((firstContact));
-	   }
-}
