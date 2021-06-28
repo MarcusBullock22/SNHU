239d740adbb82be3db2ce016d9889e49036b34c8
Marcus Bullock <37095667+Mrtbull@users.noreply.github.com>
2021-06-27 22:35:01 -0400

    Add files via upload

---
+++ CS-360/Marcus_Bullock_androidinto/app/src/test/java/com/example/marcus_bullock_androidinto/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.marcus_bullock_androidinto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
